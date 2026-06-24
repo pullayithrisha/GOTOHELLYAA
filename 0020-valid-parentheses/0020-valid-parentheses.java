632685438f61bc665ec3fb941b3da9f9ea66e28b
pullayi thrisha <156199815+pullayithrisha@users.noreply.github.com>
2026-06-24 23:12:38 +0530

    Time: 4 ms (38.78%), Space: 43.4 MB (25.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2==0)
+        {
+             Stack<Character> st=new Stack<>();
+                for(int i=0;i<s.length();i++)
+                {
+                    char ch=s.charAt(i);
+                    if(ch=='(' || ch=='{' || ch=='[')
+                    {
+                        st.push(ch);
+                    }
+                    else
+                    {
+                        if( (!st.isEmpty()) && ((ch==')' && st.peek()=='(') ||(ch=='}' && st.peek()=='{') || (ch==']' && st.peek()=='[') ))
+                        {
+                            st.pop();
+                        } 
+                        else
+                        {
+                            return false;
+                        }                   
+                    }
+                }
+                if(st.isEmpty())
+                {
+                    return true;
+                }
+        }  
+        return false;
+    }
+}
