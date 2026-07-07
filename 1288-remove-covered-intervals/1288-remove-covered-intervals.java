ed52a8d93c74cc4a00b491fa7e8cb18ec91e7243
pullayi thrisha <156199815+pullayithrisha@users.noreply.github.com>
2026-07-07 19:37:46 +0530

    Time: 41 ms (5.05%), Space: 47 MB (11.33%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -9,22 +9,37 @@ class Solution {
     }
     public int removeCoveredIntervals(int[][] intervals) {
         int res=0;
-        for(int i=0;i<intervals.length;i++)
-        {
-            boolean covered=false;
-            for(int j=0;j<intervals.length;j++)
-            {
-                if(i!=j && covered(intervals[i],intervals[j]))
-                {
-                    covered=true;
-                    break;
-                }
-            }
-            if(!covered)
-            {
-                res++;
-            }
+        // for(int i=0;i<intervals.length;i++)
+        // {
+        //     boolean covered=false;
+        //     for(int j=0;j<intervals.length;j++)
+        //     {
+        //         if(i!=j && covered(intervals[i],intervals[j]))
+        //         {
+        //             covered=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!covered)
+        //     {
+        //         res++;
+        //     }
+        // }
+        Arrays.sort(intervals,(a,b)->{
+        if(a[0]==b[0]) return Integer.compare(b[1],a[1]);
+        return Integer.compare(a[0],b[0]);
+     });  
+        for (int i = 0; i < intervals.length; i++) {
+    System.out.println(java.util.Arrays.toString(intervals[i]));
+}
+        int maxEnd=0;
+
+     for(int[] i:intervals){
+        if(i[1]>maxEnd){
+            res++;
+            maxEnd=i[1];
         }
+     }
         return res;
     }
 }
