dd353d08711a04ac84bb7116eec46aeacaa1ab5a
pullayi thrisha <156199815+pullayithrisha@users.noreply.github.com>
2026-07-09 10:35:47 +0530

    Time: 0 ms (100%), Space: 46.6 MB (16.55%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -13,37 +13,49 @@ class Solution {
          if (head == null || head.next == null) {
             return head;
         }
-        ListNode temp=head;
-        ArrayList<Integer> a=new ArrayList<>();
-        while(temp!=null&&temp.next!=null)
+       // ListNode temp=head;
+        // ArrayList<Integer> a=new ArrayList<>();
+        // while(temp!=null&&temp.next!=null)
+        // {
+        //     a.add(temp.val);
+        //     temp=temp.next.next;
+        // }
+        // if(temp!=null){
+        //         a.add(temp.val);
+        //         temp=temp.next;
+        // } 
+        // temp=head.next;
+        // while(temp!=null&&temp.next!=null)
+        // {
+        //     a.add(temp.val);
+        //     temp=temp.next.next;
+        // }
+        // if(temp!=null){
+        //         a.add(temp.val);
+        //         temp=temp.next;
+        // }
+        // System.out.println("a:"+a);
+        // int i=0;
+        // temp=head;
+        // while(temp!=null)
+        // {
+        //     temp.val=a.get(i);
+        //     temp=temp.next;
+        //     i++;
+        // }
+        // return head;
+
+        ListNode even=head.next;
+        ListNode odd=head;
+        ListNode evenhead=even;
+        while(even!=null&&even.next!=null)
         {
-            a.add(temp.val);
-            temp=temp.next.next;
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
         }
-        if(temp!=null){
-                a.add(temp.val);
-                temp=temp.next;
-        } 
-        temp=head.next;
-        while(temp!=null&&temp.next!=null)
-        {
-            a.add(temp.val);
-            temp=temp.next.next;
-        }
-        if(temp!=null){
-                a.add(temp.val);
-                temp=temp.next;
-        }
-        System.out.println("a:"+a);
-        int i=0;
-        temp=head;
-        while(temp!=null)
-        {
-            temp.val=a.get(i);
-            temp=temp.next;
-            i++;
-        }
+        odd.next=evenhead;
         return head;
-        
     }
 }
