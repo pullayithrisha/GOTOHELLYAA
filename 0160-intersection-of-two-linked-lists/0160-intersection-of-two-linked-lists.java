0199ac21142455f9707823319775f7da7323d57e
pullayi thrisha <156199815+pullayithrisha@users.noreply.github.com>
2026-07-11 15:07:56 +0530

    Time: 1 ms (99.93%), Space: 52.9 MB (44.63%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,23 +10,53 @@
  * }
  */
 public class Solution {
+    public ListNode collison(ListNode l,ListNode s,int n)
+    {
+        while(n>0)
+        {
+            n--;
+            l=l.next;
+        }
+        while(l!=s)
+        {
+            l=l.next;
+            s=s.next;
+        }
+        return l;
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Integer> hm=new HashMap<>();
-        ListNode t1=headA;
-        while(t1!=null)
+        // HashMap<ListNode,Integer> hm=new HashMap<>();
+        // ListNode t1=headA;
+        // while(t1!=null)
+        // {
+        //     hm.put(t1,1);
+        //     t1=t1.next;
+        // }
+        // t1=headB;
+        // while(t1!=null)
+        // {
+        //     if(hm.containsKey(t1))
+        //     {
+        //         return t1;
+        //     }
+        //     t1=t1.next;
+        // }
+        // return null;
+        ListNode t=headA;
+        int n1=0;
+        int n2=0;
+        while(t!=null)
         {
-            hm.put(t1,1);
-            t1=t1.next;
+            n1++;
+            t=t.next;
         }
-        t1=headB;
-        while(t1!=null)
+        t=headB;
+        while(t!=null)
         {
-            if(hm.containsKey(t1))
-            {
-                return t1;
-            }
-            t1=t1.next;
+            n2++;
+            t=t.next;
         }
-        return null;
+        if(n1>n2) { return collison(headA,headB,n1-n2);}
+         return collison(headB,headA,n2-n1);
     }
 }
